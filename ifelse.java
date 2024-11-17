1a5187ef2a21481e5384c539253ea2d4e41ce999
kanchan2203 <kanchanchandel022@gmail.com>
2024-11-17 13:59:20 +0530

    Add files via upload
    first commit

---
+++ ifelse.java
@@ -0,0 +1,12 @@
+public class ifelse {
+    public static void main(String[] args) {
+        if (5>3) {
+            System.out.println(1);
+        }
+        if (3>2) {
+            System.out.println(2);
+        }
+      
+    }
+    
+}
